ed928c84018fbd5b515d87069f66c00ac628fe55
VighneshBilgi <vhbilgi@gmail.com>
2022-06-06 02:46:50 +0530

    Added: UC1_First_Name

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -1,8 +1,37 @@
 package com.bridgelabz.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
 
+    public void firstName(String firstName) {
+
+        String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
+
+        Pattern pattern = Pattern.compile(firstNamePattern);
+        Matcher matcher = pattern.matcher(firstName);
+        boolean result = matcher.matches();
+
+        if(result == true){
+            System.out.println("First Name is valid.");
+        }
+
+        else
+            System.out.println("First Name is not valid.");
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Welcome to User Registration Problem:");
+
+        UserRegistration ur = new UserRegistration();
+
+        System.out.println("Enter First Name:");
+        String firstName = scanner.next();
+        ur.firstName(firstName);
+
     }
 }
