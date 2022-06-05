c90dac24ef76b44ef9e186aae288d9ca03bc3f14
VighneshBilgi <vhbilgi@gmail.com>
2022-06-06 03:04:04 +0530

    Added: UC5_Password_Rule_1

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -6,7 +6,6 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-
     public void firstName(String firstName) {
 
         String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
@@ -66,6 +65,24 @@ public class UserRegistration {
         }
     }
 
+    public void passwordRule1(String password){
+
+        String PasswordPattern="[a-zA-Z0-9~!@#$]{8,}";
+
+        Pattern pattern4=Pattern.compile(PasswordPattern);
+        Matcher matcher4=pattern4.matcher(password);
+        Boolean result4=matcher4.matches();
+
+        if(result4==true){
+            System.out.println("Password is valid");
+        }else{
+            System.out.println("Invaild, minimum 8 characters required.");
+        }
+
+    }
+
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -84,9 +101,13 @@ public class UserRegistration {
         String email = scanner.next();
         ur.email(email);
 
-        System.out.println("Enter your mobile number");
+        System.out.println("Enter your Mobile number:");
         String mobileNumber = scanner.next();
         ur.mobileNumber(mobileNumber);
+
+        System.out.println("Enter your Password:");
+        String password=scanner.next();
+        ur.passwordRule1(password);
 
     }
 }
