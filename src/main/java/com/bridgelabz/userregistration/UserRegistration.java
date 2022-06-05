36cfbb18a62e056b4f38863667ae5d0ed75323ad
VighneshBilgi <vhbilgi@gmail.com>
2022-06-06 02:54:59 +0530

    Added: UC3_Email

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -6,6 +6,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 
+
     public void firstName(String firstName) {
 
         String firstNamePattern = "[A-Z]{1}[a-z]{2,}";
@@ -36,6 +37,20 @@ public class UserRegistration {
             System.out.println("Last Name is not valid.");
     }
 
+    public void email(String email){
+        String emailPattern= "[a-zA-Z0-9]+[.+-]{0,1}[0-9a-zA-Z]*@[a-z0-9]+[.][a-z]{2,4}[.a-z]{0,4}";
+
+        Pattern pattern2=Pattern.compile(emailPattern);
+        Matcher matcher2=pattern2.matcher(email);
+        boolean result2=matcher2.matches();
+
+        if(result2== true){
+            System.out.println("Email is valid.");
+        }else{
+            System.out.println("Email is not valid.");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -49,5 +64,9 @@ public class UserRegistration {
         System.out.println("Enter Last Name:");
         String lastName = scanner.next();
         ur.lastName(lastName);
+
+        System.out.println("Enter your Email:");
+        String email = scanner.next();
+        ur.email(email);
     }
 }
